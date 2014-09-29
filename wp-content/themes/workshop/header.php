9be333fb88b8a59899d6096d84851103da18f212
accacio <accacio@apiki.com>
2014-09-29 14:56:36 -0300

    add tags google analytics

--- wp-content/themes/workshop/header.php
+++ wp-content/themes/workshop/header.php
@@ -19,8 +19,18 @@ global $wp_theme;
 		<script src="<?php echo esc_url( $wp_theme->template_url ); ?>/assets/javascripts/augment.min.js"></script>
 	<![endif]-->
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-55262703-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+
 	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ) ?>?v=<?php echo esc_attr( filemtime( $wp_theme->template_directory  . '/style.css' ) ); ?>">
-	<?php wp_head(); ?>
+	<?php wp_head(); ?>	
 </head>
 <body <?php $wp_theme->routes->module_js(); ?>>
 
