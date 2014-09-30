064635638c0bd93f25702f3f3104f559407e1087
accacio <accacio@apiki.com>
2014-09-30 01:08:51 -0300

    alter latitute longitute

--- wp-content/themes/workshop/template-parts/template-part-maps.php
+++ wp-content/themes/workshop/template-parts/template-part-maps.php
@@ -11,8 +11,8 @@ global $wp_theme;
 
 <div class="map" id="local">
 	<div data-component-map style="width:100%; height:400px;"
-	     data-attr-lat="-18.8732072"
-	     data-attr-lng="-41.9594773"
+	     data-attr-lat="-18.8394442"
+	     data-attr-lng="-41.9060897"
 	     data-attr-title="Univale - MG"
 	     data-attr-icon="<?php echo esc_url( $wp_theme->template_url ); ?>/assets/images/pointer.png">
 		<span class="custom-spinner"></span>
